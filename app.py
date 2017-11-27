90a06a53fc3d8c9e12070dbd61dc0faf2a85bc3b
Prateek Arora <1192prateek@gmail.com>
2017-11-27 01:54:16 +0000

    Adding Error Handling to the code
    This commit introduces error handling to the code and whenever
    new errors are discovered, a new class in errors.py would have to
    be created and handled in app.py

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from flask import request
+from flask import jsonify
 import subprocess
+from error import InvalidUsage
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
 
 @app.route('/drain_node')
 def drain_node():
@@ -21,6 +25,8 @@ def drain_node():
 @app.route('/resume_node')
 def resume_node():
     hostname = request.args.get('hostname')
+    if not hostname:
+        raise InvalidUsage('Hostname is Null', status_code=500)
     query_string = 'ansible-playbook -i inventory/hosts --extra-vars compute="'+hostname+'" /root/slurm_interactor/roles/common/tasks/resume-compute.yml'
     proc =  subprocess.Popen(query_string, stdout=subprocess.PIPE,stderr=subprocess.STDOUT, shell=True)
     out = proc.communicate()
